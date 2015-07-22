d331b14271dacb095846e55cad09b9fe9e23c442
Juan Roa <jdroa92@gmail.com>
2015-07-22 11:17:09 -0500

    Adding tarifador calculator with calc.js and styles/html5

---
+++ www/js/calc.js
@@ -0,0 +1,8 @@
+function calcula(operacion){
+    var a = 300
+    var b = 60
+    var distance = document.calc.distance.value
+    var timer = document.calc.timer.value
+    var result = eval((distance*a) + (timer*b))
+    document.calc.resultado.value = result
+  }
